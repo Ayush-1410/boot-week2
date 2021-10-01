4c04934750813a6ca133827087584e47c1ea095d
Ayush-1410 <63532654+Ayush-1410@users.noreply.github.com>
2021-10-01 12:33:38 +0530

    Update Ques3.js

--- ES6(Session 1)/Ques3.js
+++ ES6(Session 1)/Ques3.js
@@ -2,15 +2,15 @@
 
 
 const song = {
-		name: 'Dying to live',
+	name: 'Dying to live',
     artist: 'Tupac',
     featuring: 'Biggie Smalls'
 };
 
 const markup = `
-		<div class="song">
-    		<p> ${song.name} - ${song.artist} (Featuring ${song.featuring}) </p>
- 		</div>
+	<div class="song">
+    	<p> ${song.name} - ${song.artist} (Featuring ${song.featuring}) </p>
+    </div>
  `;
 
 document.body.innerHTML = markup;
