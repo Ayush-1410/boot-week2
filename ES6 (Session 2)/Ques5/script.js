024ccdb292db1fd681f31ea53446c87032c7aa82
Ayush-1410 <63532654+Ayush-1410@users.noreply.github.com>
2021-10-04 23:29:56 +0530

    Update script.js

--- ES6 (Session 2)/Ques5/script.js
+++ ES6 (Session 2)/Ques5/script.js
@@ -1,4 +1,4 @@
-const lib =  require('./lib');
+import { area_rec, area_cir, area_cylinder } from "./lib.js";
 
 // rectangle
 let length = 10;
@@ -14,6 +14,6 @@ let height = 10;
 // Calling the functions 
 // defined in the lib module
 
-lib.area_rec(length, breadth);
-lib.area_cir(pi, radius);
-lib.area_cylinder(pi, radius, height);
+area_rec(length, breadth);
+area_cir(pi, radius);
+area_cylinder(pi, radius, height);
