d09030acf9b5c1c5e1c8c0bb10e156ef7c975def
Ayush-1410 <63532654+Ayush-1410@users.noreply.github.com>
2021-10-04 19:32:03 +0530

    Create Ques2.js

---
+++ ES6 (Session 2)/Ques2.js
@@ -0,0 +1,7 @@
+// Find the possible combinations of a string and store them in a MAP
+
+let map = new Map();
+
+map.set('1', 'str');   // with a string key
+map.set(1, 'num');     // with a numeric key
+map.set(true, 'bool'); // with a boolean key
