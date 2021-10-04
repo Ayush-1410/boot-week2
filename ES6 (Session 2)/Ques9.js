7810a41d696c8b562262265edccaf58fb67aa9bb
Ayush-1410 <63532654+Ayush-1410@users.noreply.github.com>
2021-10-04 20:46:02 +0530

    Create Ques9.js

---
+++ ES6 (Session 2)/Ques9.js
@@ -0,0 +1,16 @@
+// Implementation of Set 
+
+let names = new Set(['A','B','C','D']);
+
+console.log(names.size)   // 4
+
+
+// Implementation of Map
+
+let days = new Map();
+
+days.set('1', 'Monday');
+days.set('2', 'Tuesday');
+days.set('3', 'Wednesday');
+
+console.log(days.size);   // 3
